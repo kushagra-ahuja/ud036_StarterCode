21ff8e4c691cf01a8237d87e80fa3b0cfa973608
kushagra-ahuja <35661884+kushagra-ahuja@users.noreply.github.com>
2018-01-22 23:36:36 +0530

    Create movie.py

---
+++ movie.py
@@ -0,0 +1,12 @@
+class Movie():
+#Constructor with user defined arguments
+    def __init__(self,movie_title, movie_storyline, movie_image,movie_youtubeTrailer):
+        self.title= movie_title
+        self.storyline=movie_storyline
+        self.poster_image_url= movie_image
+        self.trailer_youtube_url=movie_youtubeTrailer
+		
+        
+
+        
+        
